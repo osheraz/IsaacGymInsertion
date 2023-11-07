542eb46bbc0bdca8570e4e2f50bb53be4a98b6d9
Osher Azulay <oa348@ilab2.cs.rutgers.edu>
2023-11-07 12:28:17 -0500

    update gen_grasp

--- isaacgyminsertion/scripts/gen_grasp.sh
+++ isaacgyminsertion/scripts/gen_grasp.sh
@@ -4,4 +4,5 @@ python trainV2.py task=FactoryTaskInsertionTactile headless=True pipeline=gpu \
 task.env.numEnvs=16 \
 task.env.tactile=True \
 train.ppo.tactile_info=True \
+task.tactile.half_image=False \
 train.ppo.priv_info=True
