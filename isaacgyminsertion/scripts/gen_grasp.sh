6029ce23c65f2543c0f19601d1f8d926a1091594
Osher Azulay <oa348@ilab2.cs.rutgers.edu>
2023-11-20 14:54:07 -0500

    with new grasp pos

--- isaacgyminsertion/scripts/gen_grasp.sh
+++ isaacgyminsertion/scripts/gen_grasp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python trainV2.py task=FactoryTaskInsertionTactile headless=False pipeline=gpu \
-task.env.numEnvs=2 \
+python trainV2.py task=FactoryTaskInsertionTactile headless=True pipeline=gpu \
+task.env.numEnvs=32 \
 task.sim.substeps=5 \
 task.env.tactile=True \
 task.env.tactile_display_viz=False \
@@ -11,4 +11,4 @@ task.tactile.tacto.height=224 \
 train.ppo.tactile_info=True \
 task.env.smooth_force=False \
 task.tactile.half_image=False \
-train.ppo.priv_info=True \
+train.ppo.priv_info=True
