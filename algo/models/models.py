a79c0f48fb73627c00a8c64ac5e2e2baa48222c7
dhruvmetha <dhruv.metha@gmail.com>
2023-11-21 20:43:22 -0500

    minor fix

--- algo/models/models.py
+++ algo/models/models.py
@@ -184,7 +184,7 @@ class ActorCritic(nn.Module):
         mu, logstd, value, latent, _ = self._actor_critic(obs_dict)
         return mu, latent
 
-    def _actor_critic(self, obs_dict, display=True):
+    def _actor_critic(self, obs_dict, display=False):
         obs = obs_dict['obs']
         extrin, extrin_gt = None, None
 
