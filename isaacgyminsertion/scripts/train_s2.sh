0982d3ad60a3698e6c4c4de222af990e7f8ff09f
dhruvmetha <dhruv.metha@gmail.com>
2023-11-22 12:03:34 -0500

    dhruv changes to run train_s2

--- isaacgyminsertion/scripts/train_s2.sh
+++ isaacgyminsertion/scripts/train_s2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 GPUS=${1:-0}
 SEED=${2:-42}
-CACHE=${3:-ext}
-NUM_ENVS=${4:-32}
+CACHE=${3:-"11-21-23/18-16-32"}
+NUM_ENVS=${4:-2}
 HEADLESS=${5:-True}
 
 
@@ -21,6 +21,9 @@ task.env.numEnvs=${NUM_ENVS} \
 offline_training=False \
 offline_training_w_env=False \
 task.env.tactile=True \
+task.env.numObsHist=5 \
+task.env.numObservations=24 \
+task.env.numStates=16 \
 task.env.tactile_history_len=5 \
 task.tactile.tacto.width=224 \
 task.tactile.tacto.height=224 \
@@ -29,12 +32,13 @@ task.tactile.decoder.height=224 \
 task.env.tactile_wrt_force=True \
 task.tactile.decoder.num_channels=1 \
 task.tactile.half_image=True \
-task.env.compute_contact_gt=True \
+task.env.compute_contact_gt=False \
 task.env.smooth_force=True \
 train.ppo.tactile_info=True \
 train.ppo.obs_info=False \
 train.algo=ExtrinsicAdapt \
-train.ppo.priv_info=True train.ppo.extrin_adapt=True \
+train.ppo.priv_info=True \
+train.ppo.extrin_adapt=True \
 train.ppo.output_name="${CACHE}" \
 checkpoint="${C}" \
 ${EXTRA_ARGS}
